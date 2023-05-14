8e2e77d757681d0b10fa0927160471ebd6462efe
Nathan Kelly <nathan-kelly@hotmail.co.uk>
2023-05-14 22:41:36 +0100

    Finish up component rendering and styling

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,13 +1,36 @@
 import Wrapper from "./components/Wrapper";
 import Screen from "./components/Screen";
+import ButtonBox from "./components/ButtonBox";
+import Button from "./components/Button";
 
+const btnValues = [
+  ["C", "+-", "%", "/"],
+  [7, 8, 9, "X"],
+  [4, 5, 6, "-"],
+  [1, 2, 3, "+"],
+  [0, ".", "="],
+];
 
 const App = () => {
   return (
     <Wrapper>
-      <Screen>
-
-      </Screen>
+      <Screen/>
+      <ButtonBox>
+        {
+          btnValues.flat().map((btn, i) => {
+            return (
+              <Button
+                key={i}
+                className={btn === "=" ? "equals" : ""}
+                value={btn}
+                onClick={() => {
+                  console.log(`${btn} clicked!`);
+                }}
+              />
+            );
+          })
+        }
+      </ButtonBox>
     </Wrapper>
   );
 };
