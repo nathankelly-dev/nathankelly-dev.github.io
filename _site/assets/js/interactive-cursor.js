45e213854788899ce6e630660b9048e9e881cbf4
Nathan Kelly <nathan-kelly@hotmail.co.uk>
2023-05-19 22:16:12 +0100

    Adjust js so the custom cursor is centered properly

--- _site/assets/js/interactive-cursor.js
+++ _site/assets/js/interactive-cursor.js
@@ -3,6 +3,6 @@ console.log(cursor)
 
 document.onmousemove = ((e) => {
     console.log('waahhhhh')
-    cursor.style.left = e.pageX + 'px';
-    cursor.style.top = e.pageY + 'px';
+    cursor.style.left = e.pageX + -13 + 'px';
+    cursor.style.top = e.pageY + -13 + 'px';
 })
