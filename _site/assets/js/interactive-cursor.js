27c1f1d1016e8c8b48e2ecdf573c63cdb263e5a0
Nathan Kelly <nathan-kelly@hotmail.co.uk>
2023-05-19 22:20:52 +0100

    Make cursor point events none to allow for interactivity

--- _site/assets/js/interactive-cursor.js
+++ _site/assets/js/interactive-cursor.js
@@ -2,7 +2,6 @@ const cursor = document.getElementById('interactive-cursor');
 console.log(cursor)
 
 document.onmousemove = ((e) => {
-    console.log('waahhhhh')
     cursor.style.left = e.pageX + -13 + 'px';
     cursor.style.top = e.pageY + -13 + 'px';
 })
