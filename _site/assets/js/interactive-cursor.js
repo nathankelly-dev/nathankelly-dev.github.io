b84db31cf21d9d0ff44fc9b73946ded6a48b24ad
Nathan Kelly <nathan-kelly@hotmail.co.uk>
2023-05-19 22:48:52 +0100

    Update cursor to change colour and innerhtml on slider hover

--- _site/assets/js/interactive-cursor.js
+++ _site/assets/js/interactive-cursor.js
@@ -1,7 +1,38 @@
 const cursor = document.getElementById('interactive-cursor');
-console.log(cursor)
+const sliderHover = document.querySelectorAll('.section--slider .slider__inner');
 
 document.onmousemove = ((e) => {
     cursor.style.left = e.pageX + -13 + 'px';
     cursor.style.top = e.pageY + -13 + 'px';
 })
+
+document.onmousedown = () => {
+    cursor.style.transform = 'scale(1.8)';
+}
+
+document.onmouseup = () => {
+    cursor.style.transform = 'scale(1)';
+}
+
+sliderHover.forEach((target) => {
+    target.addEventListener('mouseover', () => {
+        cursor.style.transition = 'color 0.1s ease-in-out, transform 0.1s ease-in-out';
+        setTimeout(() => {
+          cursor.style.backgroundColor = 'blue';
+          cursor.innerHTML = '>';
+          cursor.style.transform = 'scale(1.8)';
+        }, 10);
+    });
+});
+  
+sliderHover.forEach((target) => {
+    target.addEventListener('mouseleave', () => {
+        cursor.style.transition = 'color 0.1s ease-in-out, transform 0.1s ease-in-out';
+        setTimeout(() => {
+          cursor.style.borderColor = 'white';
+          cursor.style.backgroundColor = 'transparent';
+          cursor.innerHTML = '';
+          cursor.style.transform = 'scale(1)';
+        }, 10);
+    })
+});
