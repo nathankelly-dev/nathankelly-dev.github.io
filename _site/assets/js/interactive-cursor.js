f7c33dbbb6ca5781b9cf129bdd91ea04a744b51b
Nathan Kelly <nathan-kelly@hotmail.co.uk>
2023-05-19 22:54:55 +0100

    Make interactive when on hero section

--- _site/assets/js/interactive-cursor.js
+++ _site/assets/js/interactive-cursor.js
@@ -1,4 +1,5 @@
 const cursor = document.getElementById('interactive-cursor');
+const heroSectionHover = document.querySelector('.section--hero');
 const sliderHover = document.querySelectorAll('.section--slider .slider__inner');
 
 document.onmousemove = ((e) => {
@@ -14,13 +15,34 @@ document.onmouseup = () => {
     cursor.style.transform = 'scale(1)';
 }
 
+heroSectionHover.addEventListener('mouseover', () => {
+    cursor.style.transition = 'color 0.1s ease-in-out, transform 0.1s ease-in-out';
+    setTimeout(() => {
+        cursor.style.backgroundColor = 'black';
+        cursor.innerHTML = 'Scroll down!';
+        cursor.style.height = '100px';
+        cursor.style.width = '100px';
+    }, 10);
+});
+  
+heroSectionHover.addEventListener('mouseleave', () => {
+    cursor.style.transition = 'color 0.1s ease-in-out, transform 0.1s ease-in-out';
+    setTimeout(() => {
+        cursor.style.backgroundColor = 'transparent';
+        cursor.innerHTML = '';
+        cursor.style.height = '15px';
+        cursor.style.width = '15px';
+    }, 10);
+});
+
 sliderHover.forEach((target) => {
     target.addEventListener('mouseover', () => {
         cursor.style.transition = 'color 0.1s ease-in-out, transform 0.1s ease-in-out';
         setTimeout(() => {
-          cursor.style.backgroundColor = 'blue';
-          cursor.innerHTML = '>';
-          cursor.style.transform = 'scale(1.8)';
+          cursor.style.backgroundColor = 'black';
+          cursor.innerHTML = 'Drag me!';
+          cursor.style.height = '100px';
+          cursor.style.width = '100px';
         }, 10);
     });
 });
@@ -29,10 +51,10 @@ sliderHover.forEach((target) => {
     target.addEventListener('mouseleave', () => {
         cursor.style.transition = 'color 0.1s ease-in-out, transform 0.1s ease-in-out';
         setTimeout(() => {
-          cursor.style.borderColor = 'white';
           cursor.style.backgroundColor = 'transparent';
           cursor.innerHTML = '';
-          cursor.style.transform = 'scale(1)';
+          cursor.style.height = '15px';
+          cursor.style.width = '15px';
         }, 10);
     })
 });
